dfed01a44bc98cf3dd696dd0b2ac6fd986ab68eb
philipobiorah <philip.obiorah@outlook.com>
2020-01-20 19:14:17 +0100

    added config files

--- db.php
+++ db.php
@@ -1,5 +1,20 @@
+
 <?php
-$con =mysqli_connect("localhost", "root", "", "LoginSystem");
+  $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"],1);
+
+//   mysql_connect($server, $username, $password);
+
+//   mysql_select_db($db);
+?>
+
+
+<?php
+$con =mysqli_connect($server, $username, $password, $db);
 //check connection
 if (mysqli_connect_errno()){
     echo "Failed to connect ot MySQL: " . mysqli_connect_errno();
@@ -7,3 +22,4 @@ if (mysqli_connect_errno()){
 
 
 ?>
+
